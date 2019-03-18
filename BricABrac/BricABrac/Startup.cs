768fe4de509688c17ea1495f503da4b18cf8eff6
Royejul <julien.feuillad@he-arc.ch>
2019-03-18 11:35:32 +0100

    Login, logout, register page

--- BricABrac/BricABrac/Startup.cs
+++ BricABrac/BricABrac/Startup.cs
@@ -42,6 +42,16 @@ namespace BricABrac
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,5 +76,6 @@ namespace BricABrac
 
             app.UseMvc();
         }
+
     }
 }
